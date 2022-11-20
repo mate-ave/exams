d32b69d388e2ecce37e77a667d0064c0bd83adf1
Emanuel <Emanuel Rodrguez>
2022-11-20 18:32:49 -0300

    Verano y recus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Usar BrowserRouter para el servidor local y HashRouter para el servidor en GHPages
 root.render(
   // <BrowserRouter>
     <HashRouter basename="/">
